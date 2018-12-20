2f0d7682183458e2e738a82228854bce980949f1
Haijin Development <haijin.development@gmail.com>
2018-12-20 09:32:29 -0300

    Implementing Mysql.

--- tests/Mysql/QueryBuilder/MysqlQueryTestBase.php
+++ tests/Mysql/QueryBuilder/MysqlQueryTestBase.php
@@ -10,10 +10,30 @@ class MysqlQueryTestBase extends \PHPUnit\Framework\TestCase
     {
         parent::setUp();
 
+        $this->drop_tables();
+        $this->create_tables();
+        $this->populate_tables();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        $this->drop_tables();
+    }
+
+    protected function drop_tables()
+    {
         $db = new \mysqli( "127.0.0.1", "haijin", "123456", "haijin-persistency" );
         $db->query(
-            "DROP TABLE users IF EXISTS;"
+            "DROP TABLE users;"
         );
+        $db->close();
+    }
+
+    protected function create_tables()
+    {
+        $db = new \mysqli( "127.0.0.1", "haijin", "123456", "haijin-persistency" );
         $db->query(
             "CREATE TABLE `haijin-persistency`.`users` (
                 `id` INT NOT NULL AUTO_INCREMENT,
@@ -22,6 +42,12 @@ class MysqlQueryTestBase extends \PHPUnit\Framework\TestCase
                 PRIMARY KEY (`id`)
             );"
         );
+        $db->close();
+    }
+
+    protected function populate_tables()
+    {
+        $db = new \mysqli( "127.0.0.1", "haijin", "123456", "haijin-persistency" );
         $db->query(
             "INSERT INTO users VALUES ( 1, 'Lisa', 'Simpson' );"
         );
@@ -30,17 +56,6 @@ class MysqlQueryTestBase extends \PHPUnit\Framework\TestCase
         );
         $db->query(
             "INSERT INTO users VALUES ( 3, 'Maggie', 'Simpson' );"
-        );
-        $db->close();
-    }
-
-    public function tearDown()
-    {
-        parent::tearDown();
-
-        $db = new \mysqli( "127.0.0.1", "haijin", "123456", "haijin-persistency" );
-        $db->query(
-            "DROP TABLE users IF EXISTS;"
         );
         $db->close();
     }
